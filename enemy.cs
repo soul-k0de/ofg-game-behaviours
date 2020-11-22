1ed596ba52db6e1ed13d58840436c1b63920fe68
Max <mx.a.kovalenko@gmail.com>
2020-11-22 21:20:28 +0200

    Reforms
    New version of enemy.cs

--- enemy.cs
+++ enemy.cs
@@ -7,12 +7,18 @@ public class enemy : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public int enemyhealth = 50;
+    public float enemySpeed = 2f;
+    public float direction = 1f; 
     int currentenemyhp;
     public Transform _tr;
-    public Transform _herotransform;
+    public GameObject coin;
     private Collider2D _coll;
     public SpriteRenderer _sprend;
     public Sprite killedslime;
+    public Transform point1;
+    public Transform point2;
+    public string enemyclass;
+    public bool gotToThePoint1;
 
     void Start()
     {
@@ -46,20 +52,22 @@ public class enemy : MonoBehaviour
         _sprend.sprite = killedslime;
         Debug.Log("Enemy died");
         Invoke("DestroyItself", 2);
+        Instantiate(coin, _tr.position, coin.transform.rotation);
         Debug.Log("Enemy destroyed");
     }
-    
+
 
     void FixedUpdate()
     {
-        if (_herotransform.position.x > _tr.position.x)
+        _rb.velocity = new Vector2(enemySpeed * Time.deltaTime * direction, _rb.velocity.y);
+        if (!Mathf.Approximately(transform.position.x, point1.transform.position.x))
         {
-            _tr.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            direction = 1f;
         }
 
-        if (_herotransform.position.x < _tr.position.x)
+        if (!Mathf.Approximately(transform.position.x, point2.transform.position.x))
         {
-            _tr.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            direction = -1f;
         }
     }
 }
